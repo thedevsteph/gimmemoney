21b26075efdcfc3f5b106573ef4630c30e61dae6
thedevsteph <thedevsteph@gmail.com>
2019-09-08 15:05:36 +0100

    first commit

---
+++ app/src/main/java/yass/stephanie/com/gimmemoney/MainActivity.kt
@@ -0,0 +1,12 @@
+package yass.stephanie.com.gimmemoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
