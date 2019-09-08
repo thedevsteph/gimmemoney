686524c3484dc39542522be74a52564eabe49562
thedevsteph <thedevsteph@gmail.com>
2019-09-08 20:45:43 +0100

    main ui done

--- app/src/main/java/yass/stephanie/com/gimmemoney/MainActivity.kt
+++ app/src/main/java/yass/stephanie/com/gimmemoney/MainActivity.kt
@@ -1,12 +1,53 @@
 package yass.stephanie.com.gimmemoney
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val continueTextView: TextView = findViewById(R.id.continue_button)
+        continueTextView.setOnClickListener {
+            var dialog = BottomSheetDialog(this, android.R.style.Theme_Black_NoTitleBar)
+            setUpConfirmTransferDialog(dialog)
+        }
+    }
+
+    private fun confirmTransferListener(dialog: BottomSheetDialog): View.OnClickListener =
+        View.OnClickListener {
+            val transferAnimationView: View? =
+                dialog.findViewById(R.id.transfer_loading_animation)
+            transferAnimationView?.apply {
+                visibility = View.VISIBLE
+                bringToFront()
+            }
+        }
+
+    private fun setUpConfirmTransferDialog(dialog: BottomSheetDialog) {
+        dialog.apply {
+            window?.apply {
+                setBackgroundDrawable(ColorDrawable(Color.argb(100, 0, 0, 0)))
+                setWindowAnimations(R.style.DialogAnimation)
+            }
+            setContentView(R.layout.confirm_money_dialog)
+            setCancelable(true)
+            setCanceledOnTouchOutside(true)
+            show()
+
+            val confirmTransferText: TextView? = dialog.findViewById(R.id.complete_transfer_button)
+            confirmTransferText?.setOnClickListener { confirmTransferListener(dialog) }
+        }
+
     }
 }
+
+
+
